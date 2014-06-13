84ac3c7eb3c1be5ac728dcc0ab58bd47b56e0764
MichielAtMove <mvanwessem@move.com>
2014-06-12 17:41:32 -0700

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public enum WidgetArguments
+        {
+            IsShowCase,
+            IsOfficeStandard,
+            IsCobroke,
+            SubmitSmartLeads,
+            PropertyStatus
+        };
+
+        private class A
+        {
+        }
+
+        class B : A
+        {
+            public class C
+            {
+            }
+
+            public static string getname<T>()
+            {
+                return typeof (T).FullName;
+            }
+        }
+
+        public class D
+        {
+            public int b;
+            public int c;
+        }
+
+        private static IEnumerable<FieldInfo> GetPublicFields(Type type)
+        {
+            var findMembers = type.FindMembers(MemberTypes.Field, BindingFlags.Public | BindingFlags.Instance, (info, criteria) => true, null);
+            var publicFields = findMembers.Cast<FieldInfo>();
+            return publicFields;
+        }
+
+        static void Main(string[] args)
+        {
+            Trace.WriteLine("");
+
+            var f = GetPublicFields(typeof(D));
+            foreach (var ff in f)
+            {
+                if (f.GetType() is int)
+                { }
+            }
+            Trace.WriteLine(B.getname<B.C>());
+            Trace.WriteLine("");
+        }
+    }
+}
